5ade90d679a26ec0f63022bcd5f98cf34cede6fc
Vlad Babii <vladbabii@users.noreply.github.com>
2021-10-09 16:10:53 +0300

    Update check.sh

--- root/check.sh
+++ root/check.sh
@@ -3,9 +3,7 @@ COUNT=$( ps aux | grep -v -e "grep" | grep "check.sh" | wc -l )
 if [ "$COUNT" == "2" ] ; then
   while true
   do
-    /bin/sh /usr/bin/set_led.sh on wifi
     /bin/sh /root/check_once.sh
-    /bin/sh /usr/bin/set_led.sh off wifi
     sleep 5
   done
 else
